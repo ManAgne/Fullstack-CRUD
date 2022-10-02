4f7ffe0c4383afa74a25fdf668885a6c4d4024ce
managne <agne.manaikaite@gmail.com>
2022-10-02 17:08:27 +0300

    feat: server folders moved from Nodejs-Express repository

---
+++ server/src/controllers/products-controller.js
@@ -0,0 +1,105 @@
+const { removeEmptyProps } = require('../helpers');
+const { createNotFoundError, sendErrorResponse } = require('../helpers/errors')
+const ProductModel = require('../models/product-model')
+
+const createProductNotFoundError = (productId) => createNotFoundError(`Product with id '${productId}' was not found`);
+
+const fetchAll = async (req, res) => {
+  const { joinBy } = req.query;
+
+  try {
+    const productDocuments = joinBy === 'categoryId'
+      ? await ProductModel.find().populate('categoryId')
+      : await ProductModel.find();
+
+    res.status(200).json(productDocuments);
+  } catch (err) { sendErrorResponse(err, res) }
+};
+
+const fetch = async (req, res) => {
+  const productId = req.params.id;
+  const { joinBy } = req.query;
+
+  try {
+    const foundProduct = joinBy === 'categoryId'
+      ? await ProductModel.findById(productId).populate('categoryId')
+      : await ProductModel.findById(productId);
+    if (foundProduct === null) throw createProductNotFoundError(productId);
+
+    res.status(200).json(foundProduct);
+  } catch (err) { sendErrorResponse(err, res) }
+};
+
+const create = async (req, res) => {
+  const newProductData = req.body;
+
+  try {
+    ProductModel.validate(newProductData);
+
+    const newProduct = await ProductModel.create(newProductData)
+
+    res.status(201).json(newProduct)
+
+  } catch (err) { sendErrorResponse(err, res) }
+};
+
+const replace = async (req, res) => {
+  const productId = req.params.id;
+  const { title, description, categoryId, img, price } = req.body;
+  const newProductData = { title, description, categoryId, img, price };
+
+  try {
+    ProductModel.validate(newProductData);
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      productId, 
+      newProductData, 
+      { new: true, runValidators: true }
+      );
+    if (updatedProduct === null) throw createProductNotFoundError(productId);
+
+    res.status(200).json(updatedProduct)
+
+  } catch (err) { sendErrorResponse(err, res) }
+};
+
+const update = async (req, res) => {
+  const productId = req.params.id;
+  const { title, description, categoryId, img, price } = req.body;
+  const newProductData = removeEmptyProps({ title, description, categoryId, img, price });
+
+  try {
+    ProductModel.validateUpdate(newProductData);
+
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      productId, 
+      newProductData, 
+      { new: true }
+      );
+
+    if (updatedProduct === null) throw createProductNotFoundError(productId);
+
+    res.status(200).json(updatedProduct)
+
+  } catch (err) { sendErrorResponse(err, res) }
+};
+
+const remove = async (req, res) => {
+  const productId = req.params.id;
+
+  try {
+    const deletedProduct = await ProductModel.findByIdAndDelete(productId);
+    if (deletedProduct === null) throw createProductNotFoundError(productId);
+
+    res.status(200).json(deletedProduct);
+  } catch (err) { sendErrorResponse(err, res) }
+};
+
+module.exports = {
+  fetchAll,
+  fetch,
+  create,
+  replace,
+  update,
+  remove,
+};
---
+++ server/src/models/product-model.js
@@ -0,0 +1,74 @@
+const { Schema, Types, model } = require('mongoose');
+const yup = require('yup');
+
+const productSchema = Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+}, {
+    timeStamps: true
+});
+
+const productValidationSchema = yup.object().shape({
+  title: yup
+    .string().typeError('Product.title must be a string')
+    .required('Product.title is required'),
+  description: yup
+    .string().typeError('Product.description must be a string')
+    .required('Product.description is required'),
+  categoryId: yup
+    .string().typeError('Cup.categoryId must be a string')
+    .test(
+      'is-mongo-object-id',
+      'Product.categoryId must be valid MongoDB object Id',
+      Types.ObjectId.isValid
+    )
+    .required('Product.categoryId is required'),
+  img: yup
+    .string().typeError('Product.img must be a string')
+    .required('Product.img is required'),
+  price: yup
+    .number().typeError('Product.price must be a number')
+    .required('Product.price is required')
+    .positive('Product.price must be positive')
+});
+
+const productUpdateValidationSchema = yup.object().shape({
+  title: yup.string().typeError('Product.title must be a string'),
+  description: yup.string().typeError('Product.description must be a string'),
+  categoryId: yup.string().typeError('Product.categoryId must be a string')
+    .test(
+      'is-mongo-object-id',
+      'Product.categoryId must be valid MongoDB object Id',
+      Types.ObjectId.isValid
+    ),
+  img: yup.string().typeError('Product.img must be a string'),
+  price: yup.number()
+    .typeError('Product.price must be a number')
+    .positive('Product.price must be positive'),
+});
+
+productSchema.statics.validate = (productData) => productValidationSchema.validateSync(productData)
+productSchema.statics.validateUpdate = (productData) => productUpdateValidationSchema.validateSync(productData)
+
+const ProductModel = model('Product', productSchema);
+
+module.exports = ProductModel;
